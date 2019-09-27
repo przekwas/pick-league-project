9cc631ffca462acf70607721485fcdcd81dca7a1
przekwas <luke@covalence.io>
2019-09-26 21:07:41 -0500

    adding router structure

--- src/server/db/queries/games.ts
+++ src/server/db/queries/games.ts
@@ -1,5 +1,14 @@
 import { knextion as knex } from '../index';
 
+const gamesForWeek = weekid => {
+	return knex('games')
+		.select('games.weekid', 'teams.id as teamid', 'teams.name')
+		.join('teams', function() {
+			this.on('teams.id', '=', 'games.home_team').orOn('teams.id', '=', 'games.away_team');
+		})
+		.where('games.weekid', weekid);
+};
+
 export default {
-    
+    gamesForWeek
 }
