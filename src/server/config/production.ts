609cca3e7b7532e5b20d02dba2e5cedd2d08cdff
przekwas <luke@covalence.io>
2019-09-26 20:58:41 -0500

    modifying config setup

---
+++ src/server/config/production.ts
@@ -0,0 +1,14 @@
+export default {
+    knex: {
+        client: process.env.KNEX_CLIENT,
+        connection: {
+            host: process.env.KNEX_HOST,
+            user: process.env.KNEX_USER,
+            password: process.env.KNEX_PASSWORD,
+            database: process.env.KNEX_SCHEMA,
+        }
+    },
+    auth: {
+        secret: process.env.AUTH_SECRET
+    }
+}
