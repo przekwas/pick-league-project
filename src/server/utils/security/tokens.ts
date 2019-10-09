486a3dda95557fbdd14773c9992be7cb30046dcd
przekwas <luke@covalence.io>
2019-10-09 00:57:08 -0500

    adding token utilities and moving interfaces

--- src/server/utils/security/tokens.ts
+++ src/server/utils/security/tokens.ts
@@ -1,10 +1,24 @@
 import * as crypto from 'crypto';
 import * as jwt from 'jsonwebtoken';
 import db from '../../db';
-import { IPayload } from '../../utils/types/interfaces';
+import { IPayload } from '../types/interfaces';
+import config from '../../config';
+
+const addDays = (date: Date, days: number) => {
+	let result = new Date(date);
+	result.setDate(result.getDate() + days);
+	return result;
+};
 
 export const createToken = async (payload: IPayload) => {
-	const result = await db.tokens.insert(payload.userid);
+	const [tokenid] = await db.tokens.insert(payload.userid);
+	payload.tokenid = tokenid;
+	payload.unique = crypto.randomBytes(32).toString('hex');
+	const token = await jwt.sign(payload, config.auth.secret);
+	let date = new Date();
+	const _expires = addDays(date, 60);
+	await db.tokens.update(token, payload.unique, _expires, payload.tokenid);
+	return token;
 };
 
 export const validateToken = () => {};
--- src/server/utils/types/interfaces.ts
+++ src/server/utils/types/interfaces.ts
@@ -11,27 +11,8 @@ export interface IGame {
 	};
 }
 
-export interface IGameTable {
-	weekid: number;
-	teamid: number;
-	name: string;
-}
-
-export interface IUserTable {
-	id: number;
-	email: string;
-	username: string;
-	hash: string;
-	role: string;
-	_created: Date;
-}
-
 export interface IPayload {
-	userid: number;
 	unique?: string;
 	tokenid?: number;
-}
-
-export interface IDbInsert {
-	insertId: number;
+	userid: number;
 }
