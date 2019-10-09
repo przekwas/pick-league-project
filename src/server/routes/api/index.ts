64f6171033a84c5758b342ecc8a5efee151f4714
przekwas <luke@covalence.io>
2019-10-09 09:18:34 -0500

    wiring routes into index router

--- src/server/routes/api/index.ts
+++ src/server/routes/api/index.ts
@@ -2,11 +2,19 @@ import { Router } from 'express';
 import { tokenCheckpoint } from '../../middleware/auth-checkpoints';
 import gamesRouter from './games';
 import teamsRouter from './teams';
+import picksRouter from './picks';
+import scoreboardRouter from './scoreboard';
+import usersRouter from './users';
+import weeksRouter from './weeks';
 
 const router = Router();
 
 router.use(tokenCheckpoint);
 router.use('/games', gamesRouter);
 router.use('/teams', teamsRouter);
+router.use('/picks', picksRouter);
+router.use('/scoreboard', scoreboardRouter);
+router.use('/users', usersRouter);
+router.use('/weeks', weeksRouter);
 
 export default router;
